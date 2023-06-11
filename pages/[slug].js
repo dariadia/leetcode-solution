5056f013e51c68d9d53c4dd7f698345102e9ea1c
Daria <daria.diachkova@mail.ru>
2023-06-11 11:58:52 +0200

    fix import

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,6 +1,6 @@
 import { getAllPosts, getDocBySlug } from '~/lib/posts';
 import markdownToHtml from '~/lib/markdown';
-import Layout from '../components/layout';
+import Layout from '~/components/Layout';
 
 export default function Doc({ meta, content }) {
   return <Layout meta={meta}>{content}</Layout>;
